57f28f31135681b6744d3547a24c0b91cd198730
Gulmira83 <gulynartaeva@gmail.com>
2020-06-26 23:45:58 +0000

    created artemis

---
+++ deployments/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "deployment_endpoint" {
+  type = "map"
+
+  default = {
+    test  = "test.artemis"
+    dev  = "dev.artemis"
+    qa   = "qa.artemis"
+    prod = "prod.artemis"
+    stage = "stage.artemis"
+  }
+}
+
+variable "deployment_image" {
+  default = "fuchicorp.com/artemis"
+}
+
+variable "deployment_environment" {
+  default = "dev"
+}
+
+variable "google_domain_name" {
+  default = "fuchicorp.com"
+}
