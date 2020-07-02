67a26bca8856a38ac196b98305853876c72cf945
Gulmira83 <59778993+Gulmira83@users.noreply.github.com>
2020-07-02 00:28:35 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -4,10 +4,9 @@ module "artemis-deploy" {
   deployment_name        = "artemis"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}" 
-  deployment_path        = "artemis"
+  deployment_path        = "isitup"
 
   template_custom_vars  = {     
-    deployment_image     = "${var.deployment_image}"
-    replicas          = "${var.replicas}"
+    deployment_image     = "${var.deployment_image}"    
   }
 }
